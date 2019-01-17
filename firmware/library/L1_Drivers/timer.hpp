eedf3746f07296a4119ad1be313b04259cc010f9
Conrad Bormann <Cbormann@live.com>
2019-01-16 18:53:12 -0800

    Initilialization and Set Timer

--- firmware/library/L1_Drivers/timer.hpp
+++ firmware/library/L1_Drivers/timer.hpp
@@ -1,20 +1,25 @@
-#include <cstdint>=
+#pragma once
+#include <cstdint>
 #include "L0_LowLevel/LPC40xx.h"
-#include "LPC40xx.hpp"
+#include "LPC40xx.h"
 
 class TimerInterface {
  public:
-  enum class Mode : uint8_t { kcont = 0x0, kreset = 0x1, kstop = 0x2 };
+
+  static LPC_TIM_TypeDef * tim_register;
+  static LPC_SC_TypeDef * sc_ptr;
+  static LPC_IOCON_TypeDef * iocon_register;
+  enum class Mode : uint8_t { kcont = 0xb0, kreset = 0xb10, kstop = 0xb100 };
   Mode operator|(Mode left, Mode right) 
   {
     return static_cast<Mode>(static_cast<int>(left) | static_cast<int>(right));
   }
   enum class TimerPort : uint8_t
   {
-    Timer1 = 0,
-    Timer2 = 1,
-    Timer3 = 2,
-    Timer4 = 3
+    kTimer1 = 0,
+    kTimer2 = 1,
+    kTimer3 = 2,
+    kTimer4 = 3
   }
   
     virtual void Initialize(TimerPort timer, const 32_t us_per_tick,
@@ -26,15 +31,66 @@ class TimerInterface {
 }
 
 class Timer : public TimerInterface {
- private:
-  uint8_t timer_port;
 
  public:
+
+  static constexpr uint8_t kTime0Mat0   = 0b011;
+  static constexpr uint8_t kPrescaleMicro   = 48;
+
+
+
+  enum class TimerPower : uint8_t
+  {
+    kTime0 = 1;
+    kTime1 = 2;
+    kTime3 = 22;
+    kTime4 = 23;
+  }
   static LPC_TIM_TypeDef* timer_peripherals[4];
 
+  constexpr Timer() : timer_(&timer_pin_), timer_pin_(1, 28) {}
+  // May not need this right now? ->
+  explicit constexpr Timer(PinInterface * Timer_pin)
+      : timer_(timer_pin), timer_pin_(Pin::CreateInactivePin())  // P1.28
+  {
+  }
+
   void Initalize(TimerPort timer, const 32_t us_per_tick,
-                 Mode current_mode) override {
+                 Mode current_mode) override 
+  {
     timer_port = static_cast<uint8_t>(timer);
     timer_peripherals[timer_port]
+    // Power up Timer 0, 2, 3, and 4 (Not sure if 
+    // all need to be started in initialize)
+    sc_ptr->PCONP |= (1<<kTime0);
+    // sc->PCONP |= (1<<kTime1);
+    // sc->PCONP |= (1<<kTime2);
+    // sc->PCONP |= (1<<kTime3);
+    // Timer 0 Match Register 0
+    timer_->SetPinFunction(kTime0Mat0);
+    timer_->SetAsAnalogMode(true);
+    timer_->SetMode(PinInterface::Mode::kInactive);
+    // set Prescale register for Prescale Counter
+    tim_register->PR |= (kPrescaleMicro << 0);
+
   }
+
+  void SetTimer(uint32_t timer)
+  {
+    // enable interrupts on timer 0 match register 0 (Reset 0, Stop 0)
+    tim_register->TIMER0 |= (1 << 0);
+
+    // Set Timer by setting match register
+    tim_register->MR0 |= (timer << 0)
+  }
+
+  uint32_t GetTimer(uint32_t timer)
+  {
+
+  }
+   private:
+   uint8_t timer_port;
+   PinInterface * timer_;
+   Pin timer_pin_;
+
 }
